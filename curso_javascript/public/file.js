4ed1e7c885febafa03bcfd3da2a17e7a3ca48304
NegritoLindo123 <cc1001447@gmail.com>
2022-10-24 14:15:14 -0500

    repaso de lo basico

--- curso_javascript/public/file.js
+++ curso_javascript/public/file.js
@@ -1,11 +1,23 @@
 /**
  * 
  */
- 
- function add (a,b){
- 
- 	return a+b;
- 
- }
+//Funciones con js
+//Declaración de la función
+function verificarEdad(edad){
+	//Condicionales 
+	if(edad >= 18){
+		var verify = "Mayor de edad";
+	}else{
+		var verify = "Menor de edad";
+	}
+	//Retornar el resultado esperado
+	return verify;
+
+}
+//Imprimir en el documento
+document.write(verificarEdad(19));
+document.write(verificarEdad(8));
+document.write(verificarEdad(17));
+document.write(verificarEdad(87));
  
  
